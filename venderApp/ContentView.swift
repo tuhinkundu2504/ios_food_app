e9bd8718f53bbbf4d9637da363858e66e82ab59a
Tuhin Kundu <53844602+tuhinkundu2504@users.noreply.github.com>
2024-08-14 12:26:19 +0530

    Initial Commit

---
+++ venderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  venderApp
+//
+//  Created by Tuhin Kundu on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ venderApp/venderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  venderAppApp.swift
+//  venderApp
+//
+//  Created by Tuhin Kundu on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct venderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
